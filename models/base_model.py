c8d139e584f18985eafee18cb36f4a313a901980
Sir-bastian <sebamadziwanzira@gmail.com>
2023-10-15 12:30:38 -0700

    AirBnB_clone

--- models/base_model.py
+++ models/base_model.py
@@ -22,11 +22,13 @@ class BaseModel():
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
+            self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
         """ Returns the string representaion of the model """
-        return f"{self.__class__.__name__}, {self.id}, {self.__dict__}"
+         return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.datetime.now()
